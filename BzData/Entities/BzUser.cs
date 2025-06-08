984ac1f6b06a0fde71f60342bfceb048444461c7
Quicro <quinten.croes@outlook.be>
2025-06-08 22:15:32 +0200

    deletede unused files

--- BzData/Entities/BzUser.cs
+++
@@ -1,7 +0,0 @@
-namespace BzData.Entities;
-
-public class BzUser
-{
-    public int Id { get; set; }
-    public string UserName { get; set; } = string.Empty;
-}
--- Codex/LinearCongruentGenerator.CLI/Program.cs
+++
@@ -1,4 +0,0 @@
-using Projects.Codex.CLI;
-
-var runArgs = args.Length == 0 ? new[] { "--count", "1" } : args;
-CliApp.Run(runArgs);
