2411728f79ac92837377ab502f6a64267ae22863
trefbaltriggerbal <quinten.croes+copilot@gmail.com>
2025-06-10 23:52:29 +0200

    Add localization with culture switch

---
+++ BlazorApp/Controllers/CultureController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp.Controllers;
+
+[Route("[controller]/[action]")]
+public class CultureController : Controller
+{
+    [HttpGet]
+    public IActionResult Set(string culture, string redirectUri)
+    {
+        if (!string.IsNullOrEmpty(culture))
+        {
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture, culture)));
+        }
+
+        return LocalRedirect(redirectUri);
+    }
+}
